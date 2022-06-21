4bad2e5103a5f22a8f99e554f432c40fee852f08
Floki <ypr_tylerkeuler@aol.com>
2022-06-21 10:38:39 -0400

    having an issue with requireing  inquirer, going to uninstall and reinstall

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+const inquirer = require('inquirer');
+
+console.log('its working');
+// inquirer
+//   .prompt([{
+//       type: 'input',
+//       name: 'input_type',
+//       message: 'what is your name?'
+//   },{
+//       type: 'input',
+//       name: 'password',
+//       message: 'whats your password'
+//   }
+
+//   ])
+//   .then((answers) => {
+//     // Use user feedback for... whatever!!
+//   })
+
+
+
+
 // declaring inquirer and fs
 
 // user question and input. 
