5821d5a48d15ffca9e42d019716113b30d45ff42
Floki <ypr_tylerkeuler@aol.com>
2022-06-20 21:03:12 -0400

    started with some sudo code for the lay out of the application and then went through the process of installing inquirer

---
+++ index.js
@@ -0,0 +1,19 @@
+// declaring inquirer and fs
+
+// user question and input. 
+
+// 1. Project Name
+// 2. Description
+// 3. License
+// 4. screen shot? 
+// 5. Installation 
+// 6. 
+// 7. 
+// 8. 
+// 9. 
+// 10. 
+
+// markdown template 
+
+// process the data and generate the readme file. 
+
